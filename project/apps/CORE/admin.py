82416549f82a842d14eabe0be75f8a8557dba326
Irina Moldavsky <frambuess@gmail.com>
2023-08-05 14:19:24 -0300

    navbar site title

--- project/apps/CORE/admin.py
+++ project/apps/CORE/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-# admin.site.site_header = "Tienda online"
-# admin.site.site_title = "Home"
+admin.site.site_header = "Tienda online"
+admin.site.site_title = "Home"
 
 admin.site.register(models.Pais)
 admin.site.register(models.Mediodepago)
