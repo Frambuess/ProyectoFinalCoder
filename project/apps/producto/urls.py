8e3bf23e89dc3392da97e4db826946791dcec456
Irina Moldavsky <frambuess@gmail.com>
2023-08-06 12:05:27 -0300

    app producto eliminar, update, list, etc..

--- project/apps/producto/urls.py
+++ project/apps/producto/urls.py
@@ -1,17 +1,40 @@
 from django.urls import path
-
-#from .views import home, index
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = "producto"
 
-
 urlpatterns = [
-    path("producto/", views.index, name="home"),
-    path("productocategoria/list/", views.productocategoria_list, name="productocategoria_list"),
-    
+    path("producto/", TemplateView.as_view(template_name="producto/index.html"), name="home"),
+    #     path("productocategoria/list/", views.productocategoria_list,
+    #          name="productocategoria_list"),
+    path("productocategoria/list/", views.ProductoCategoriaList.as_view(),
+         name="productocategoria_list"),
+    #     path("productocategoria/create/", views.productocategoria_create,
+    #          name="productocategoria_create"),
+    path("productocategoria/create/", views.ProductoCategoriaCreate.as_view(),
+         name="productocategoria_create"),
+    #     path("productocategoria/detail/<int:pk>", views.productocategoria_detail,
+    #          name="productocategoria_detail"),
+    path("productocategoria/detail/<int:pk>", views.ProductoCategoriaDetail.as_view(),
+         name="productocategoria_detail"),
+    #     path("productocategoria/update/<int:pk>", views.productocategoria_update,
+    #          name="productocategoria_update"),
+    path("productocategoria/update/<int:pk>", views.ProductoCategoriaUpdate.as_view(),
+         name="productocategoria_update"),
+    #     path("productocategoria/delete/<int:pk>", views.productocategoria_delete,
+    #          name="productocategoria_delete"),
+    path("productocategoria/delete/<int:pk>", views.ProductoCategoriaDelete.as_view(),
+         name="productocategoria_delete"),
 ]
+
+""" urlpatterns = [
+    path("producto/", TemplateView.as_view(template_name="producto/index.html"), name="home"),
+    # path("producto/", views.index, name="home"),
+    path("productocategoria/list/", views.productocategoria_list, name="productocategoria_list"),
+    path("productocategoria/create/", views.productocategoria_create, name="productocategoria_create"),  
+] """
 
 """ 
 # PRODUCTOCATEGORIA
--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,19 +1,78 @@
 # Create your views here.
 
-from django.shortcuts import render
-from . import models
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+
+from . import forms, models
 
 def index(request):
- #   contexto = {"app": "Aplicación Producto" }
     return render(request, "producto/index.html")
 
 # LIST
 
-def productocategoria_list(request):
-    categorias = models.ProductoCategoria.objects.all()
-    context = {"categorias": categorias}
-    return render(request, "producto/productocategoria_list.html", context)
+class ProductoCategoriaList(ListView):
+    model = models.ProductoCategoria
 
+# def productocategoria_list(request):
+#     categorias = models.ProductoCategoria.objects.all()
+#     context = {"object_list": categorias}
+#     return render(request, "producto/productocategoria_list.html", context)
+
+# CREATE
+
+# def productocategoria_create(request):
+#     if request.method == "POST":
+#         form = forms.ProductoCategoriaForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("producto:home")
+#     else:
+#         form = forms.ProductoCategoriaForm()
+#     return render(request, "producto/productocategoria_form.html", {"form": form})
+
+
+class ProductoCategoriaCreate(CreateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
+    success_url = reverse_lazy("producto:productocategoria_list")
+
+# DETAIL
+
+# def productocategoria_detail(request, pk):
+#     query = models.ProductoCategoria.objects.get(id=pk)
+#     return render(request, "producto/productocategoria_detail.html", {"object": query})
+
+class ProductoCategoriaDetail(DetailView):
+    model = models.ProductoCategoria
+
+class ProductoCategoriaUpdate(UpdateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
+    success_url = reverse_lazy("producto:productocategoria_list")
+
+# def productocategoria_update(request, pk):
+#     query = models.ProductoCategoria.objects.get(id=pk)
+#     if request.method == "POST":
+#         form = forms.ProductoCategoriaForm(request.POST, instance=query)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("producto:home")
+#     else:
+#         form = forms.ProductoCategoriaForm(instance=query)
+#     return render(request, "producto/productocategoria_form.html", {"form": form})
+
+
+class ProductoCategoriaDelete(DeleteView):
+    model = models.ProductoCategoria
+    success_url = reverse_lazy("producto:productocategoria_list")
 
 """ 
 from typing import Any
